6748f6a99b6e46f985877a461f9d6d2ea0750ce6
winsightnsk <winsight@mail.ru>
2022-12-26 23:22:25 +0700

    BIN-> Дааааааааааа!

--- app/src/main/java/com/example/cft_vladimir/MainActivity.kt
+++ app/src/main/java/com/example/cft_vladimir/MainActivity.kt
@@ -1,35 +1,34 @@
 package com.example.cft_vladimir
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.cft_vladimir.network.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-//import com.example.cft_vladimir.network.BinApi
-//import com.example.cft_vladimir.network.BinApiService
-//import com.example.cft_vladimir.network.retrofit
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
-import java.net.URL
+import retrofit2.http.Path
 
+object BIN {
+    var N: String = "55369138"
+}
 class MainActivity : AppCompatActivity(), Callback<JBin> {
 
-    object BIN {
-        var N: String = "55369138"
-    }
+
 
     lateinit var amtext : TextView
     lateinit var amedit : EditText
     lateinit var ambuttongo : Button
 
+
     interface ApiS {
-        @GET("55369138")
-        fun getBinAnswer() : Call<JBin>
+        @GET("{bin}")
+        fun getBinAnswer(@Path("bin") bin: String) : Call<JBin>
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,33 +45,16 @@ class MainActivity : AppCompatActivity(), Callback<JBin> {
                 .baseUrl("https://lookup.binlist.net/")
                 .build()
             val retrofitService = retro.create(ApiS::class.java)
-            val gotretro = retrofitService.getBinAnswer()
+            BIN.N = amedit.text.toString()
+            val gotretro = retrofitService.getBinAnswer(BIN.N)
             gotretro.enqueue(this)
         }
     }
 
     override fun onResponse(call: Call<JBin>, response: Response<JBin>) {
-        var s = "BIN: "
+        var s = "BIN: ${BIN.N}\n\n"
         val r = response.body() as JBin
-        s+= "\nBank:"
-        s+= "\n   city: ${r.bank.city}"
-        s+= "\n   name: ${r.bank.name}"
-        s+= "\n   url: ${r.bank.url}\n"
-        s+= "\nCountry:"
-        s+= "\n   name: ${r.country.name}"
-        s+= "\n   alpha2: ${r.country.alpha2}"
-        s+= "\n   emoji: ${r.country.emoji}"
-        s+= "\n   currency: ${r.country.currency}"
-        s+= "\n   latitude: ${r.country.latitude}"
-        s+= "\n   longitude: ${r.country.longitude}"
-        s+= "\n   numeric: ${r.country.numeric}\n"
-        s+= "\nbrand: ${r.brand}\n"
-        s+= "\nNumber:"
-        s+= "\n   length: ${r.number.length}"
-        s+= "\n   luhn: ${r.number.luhn}\n"
-        s+= "\nprepaid: ${r.prepaid}\n"
-        s+= "\nscheme: ${r.scheme}\n"
-        s+= "\ntype: ${r.type}\n"
+        s+= r.toS()
         amtext.text = s
     }
 
--- app/src/main/java/com/example/cft_vladimir/network/JBin.kt
+++ app/src/main/java/com/example/cft_vladimir/network/JBin.kt
@@ -7,5 +7,28 @@ data class JBin(
     val number: Number,
     val prepaid: Boolean,
     val scheme: String,
-    val type: String
+    val type: String,
 )
+
+fun JBin.toS(): String{
+    var s: String = "Bank:"
+    s+= "\n   city: ${this.bank.city}"
+    s+= "\n   name: ${this.bank.name}"
+    s+= "\n   url: ${this.bank.url}\n"
+    s+= "\nCountry:"
+    s+= "\n   name: ${this.country.name}"
+    s+= "\n   alpha2: ${this.country.alpha2}"
+    s+= "\n   emoji: ${this.country.emoji}"
+    s+= "\n   currency: ${this.country.currency}"
+    s+= "\n   latitude: ${this.country.latitude}"
+    s+= "\n   longitude: ${this.country.longitude}"
+    s+= "\n   numeric: ${this.country.numeric}\n"
+    s+= "\nbrand: ${this.brand}\n"
+    s+= "\nNumber:"
+    s+= "\n   length: ${this.number.length}"
+    s+= "\n   luhn: ${this.number.luhn}\n"
+    s+= "\nprepaid: ${this.prepaid}\n"
+    s+= "\nscheme: ${this.scheme}\n"
+    s+= "\ntype: ${this.type}\n"
+    return s
+}
